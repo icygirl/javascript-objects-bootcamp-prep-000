cace0fb1674a84014c749f77b3f0864bdc693bb8
Virginia Herron <inadream@gmail.com>
2018-01-09 03:55:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = new Object({'Fleetwood Mac': 'Go Your Own Way','Lionel Riche':'Hello'});
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName
+  playlist.artistName = 'songTitle';
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName) {
