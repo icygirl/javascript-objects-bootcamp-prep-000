ba22c9c59afb01e123e47476d54381591c8e9074
Virginia Herron <inadream@gmail.com>
2018-01-09 03:57:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle });
 }
 function removeFromPlaylist(playlist, artistName) {
+  playlist;
   delete playlist.artistName;
-  return playlist;
+  playlist;
 }
